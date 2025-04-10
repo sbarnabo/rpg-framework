ee728ed3f9af2c03b3b8f6a3f1565c832ec45019
sbarnabo <scott.barnabo@barnabo-connect.com>
2025-04-10 09:10:37 +0200

    Create App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Terminal from './components/Terminal'
+import './styles/catppuccin.css'
+
+function App() {
+  return (
+    <div>
+      <h1>🧠 RPG Network Terminal</h1>
+      <Terminal />
+    </div>
+  )
+}
+
+export default App
